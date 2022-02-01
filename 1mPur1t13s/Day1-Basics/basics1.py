d937f3a55f8060a95dc00c62561ee397a9c57c05
sW33t-1mPur1t13 <82957535+sW33t1mPur1t13s@users.noreply.github.com>
2022-02-01 16:22:32 +0300

    Update basics1.py

--- 1mPur1t13s/Day1-Basics/basics1.py
+++ 1mPur1t13s/Day1-Basics/basics1.py
@@ -1,6 +1,15 @@
-Variable name are unique identifies
-Variables are memory locations for storing data
+#Variable name are unique identifies
+#Variables are memory locations for storing data
 #This is a comment
 '''This is a comment too, 
   but it spans multiple lines'''
 
+#Operators
+#1. Arithmetic --> performs mathematical operations (+, -, /, *)
+#2. Assignment --> assigns values to operands (=)
+        a = 2
+        x+=4, x=x+4
+#3. Relational --> used for comparison (<, >, <=, >=, ==)
+        a = 4
+        b = 2
+        print(a==b)     #---------> returns false
